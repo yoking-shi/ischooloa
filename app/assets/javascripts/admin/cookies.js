d7874360ece5abbfb32d77879f73a2638c730323
施永坚 <syj_188888888@163.com>
2018-11-02 17:47:08 +0800

    添加忘记密码页面

--- app/assets/javascripts/admin/cookies.js
+++ app/assets/javascripts/admin/cookies.js
@@ -17,4 +17,49 @@ $(document).on('turbolinks:load', () => {
       }
     }
   })
+
+  $('#forget-password-form').validate({
+    rules: {
+      'account': {
+        required: true,
+        minlength: 6
+      },
+      'mobile': {
+        required: true
+      },
+      'password': {
+        required: true,
+        minlength: 6
+      },
+      'password_confirmation': {
+        required: true,
+        minlength: 6,
+        equalTo: '#user_password'
+      },
+      _rucaptcha: {
+        required: true
+      }
+    },
+    messages: {
+      'account': {
+        required: '请输入用户账号！',
+        minlength: '请输入最少长度为6位的用户账号!'
+      },
+      'mobile': {
+        required: '请输入手机号码！'
+      },
+      'password': {
+        required: '请输入用户密码！',
+        minlength: '请输入最少长度为6位的用户密码!'
+      },
+      'password_confirmation': {
+        required: '请输入确认密码！',
+        minlength: '请输入最少长度为6位的确认密码!',
+        equalTo: '请输入与密码相同的内容！'
+      },
+      _rucaptcha: {
+        required: '请输入验证码！'
+      }
+    }
+  })
 })
