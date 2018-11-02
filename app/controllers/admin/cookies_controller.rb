fd69686915ed15c2a1c81822baab1dc4c73c6a4c
施永坚 <syj_188888888@163.com>
2018-11-02 17:37:31 +0800

    登录

--- app/controllers/admin/cookies_controller.rb
+++ app/controllers/admin/cookies_controller.rb
@@ -8,6 +8,27 @@ class Admin::CookiesController < Admin::ApplicationController
   end
 
   def to_sign_in
+    if params[:account].present? && params[:password].present?
+      @user = User.find_by_account(params[:account])
+      if @user.present?
+        if @user.authenticate(params[:password])
+          expires = 1.hours
+          expires = 7.days if params[:remenber].present?
+          cookies[:token] = { value: @user.token, expires: expires }
+          flash[:notice] = '登录成功！'
+          redirect_to admin_root_path
+        else
+          flash[:alert] = '请填写正确的密码！'
+          redirect_to :admin_sign_in
+        end
+      else
+        flash[:alert] = '请填写正确的用户名！'
+        redirect_to :admin_sign_in
+      end
+    else
+      flash[:alert] = '请填写正确的用户名和密码！'
+      redirect_to :admin_sign_in
+    end
   end
 
   def forget_password
