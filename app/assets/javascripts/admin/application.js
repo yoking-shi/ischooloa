2a2f8f2504209fcfc820f15167c55d48e9a5680a
施永坚 <syj_188888888@163.com>
2018-11-02 16:53:25 +0800

    添加metismenu

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -2,6 +2,7 @@
 //= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
+//= require metismenu/dist/metisMenu.min.js
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
