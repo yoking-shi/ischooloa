171b08787cc9e1a7c711de71e91d54899079fc72
施永坚 <syj_188888888@163.com>
2018-11-02 14:22:20 +0800

    添加管理平台

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -1,6 +1,7 @@
 //= require jquery3
 //= require jquery_ujs
+//= require popper
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
